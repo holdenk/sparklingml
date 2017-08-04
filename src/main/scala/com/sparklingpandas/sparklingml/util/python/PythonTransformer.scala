e1a6752a68ef2f5131c88301ba4de374e9e7a415
Holden Karau <holden@us.ibm.com>
2017-08-03 22:00:28 -0700

    More progress towards registering Python UDFs from Scala

--- src/main/scala/com/sparklingpandas/sparklingml/util/python/PythonTransformer.scala
+++ src/main/scala/com/sparklingpandas/sparklingml/util/python/PythonTransformer.scala
@@ -2,18 +2,32 @@
  * Base classes to allow Scala pipeline stages to be backed
  * by Python code.
  */
+package com.sparklingpandas.sparklingml.util.python
 
-// By default most Python transformers don't support Jython evaluation.
-// Mix this trait in to allow the user to request Jython evaluation.
-// Experimental.
-trait EnableJython {
-}
+import com.sparklingpandas.sparklingml.param.HasInputCol
 
-class PythonTransformer {
-  // Python function string.
-  val pythonFunctionString: String = _
-  val pythonFunctionName: String = _
-  override def transform(input: DataFrame): DataFrame {
-    val pickled_command = gateway.
+import org.apache.spark.sql._
+import org.apache.spark.ml.Transformer
+
+trait PythonTransformer extends Transformer with HasInputCol {
+  // Name of the python function to register as a UDF
+  val pythonFunctionName: String
+
+  override def transform(input: DataFrame): DataFrame = {
+    val registrationProvider = PythonRegistration.pythonRegistrationProvider
+    val session = input.sparkSession
+    val udf = registrationProvider.registerFunction(
+      session,
+      pythonFunctionName,
+      miniSerializeParams())
+  }
+
+  /**
+   * Do you need to pass some of your parameters to Python?
+   * Put them in here and have them get evaluated with a lambda.
+   * I know its kind of sketchy -- sorry!
+   */
+  def miniSerializeParams(): String = {
+    ""
   }
 }

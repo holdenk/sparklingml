3f3ffb6af5cfbde1c79c9b4756fb18561872dacb
Holden Karau <holden@us.ibm.com>
2017-09-02 21:50:35 -0700

    Partial work towards exposing slime

--- sparklingml/dl/tensorflow.py
+++ sparklingml/dl/tensorflow.py
@@ -22,46 +22,54 @@ from tensorflowonspark import run
 from pyspark import keyword_only
 from pyspark.ml import Model
 from pyspark.ml.param import *
-from pyspark.ml.param.shared import HasInputCol, HasOutputCol
+from pyspark.ml.param.shared import HasFeatureCol, HasLabelCol
 
-class TensorFlowSlimEstimator(Model, HashInputCol, HasOutputCol):
-    stepsParam = Param(Params._dummy(),
-                       "steps", "maximum number of steps", 
-                       typeConverter=TypeConverters.toInt)
+class TensorFlowSlimEstimator(Model, HashFeatureCol, HasLabelCol):
+    """
+    >>> tfsle = TensorFlowSlimEstimator()
+    >>> tfsle.setFeatureCol("features")
+    >>> tfsle.setLabelCol("labels")
+    >>> tfsle.fromSlim("vgg", "vgg_a", "vgg_arg_scope")
+    """
+    stepsParam = Param(
+        Params._dummy(),
+        "steps", "maximum number of steps",
+        typeConverter=TypeConverters.toInt)
 
-    epochsParam = Param(Params._dummy(),
-                        "epochs", "number of epochs",
-                        typeConverter=TypeConverters.toInt)
+    epochParam = Param(
+        Params._dummy(),
+        "epoch", "number of epochs",
+        typeConverter=TypeConverters.toInt)
 
-    rdmaParam = Param(Params._dummy(),
-                      "rdma", "use rdma",
-                      typeConverter=TypeConverters.toBoolean)
+    argScopeParam = Params(
+        Params._dummy(),
+        "argScope",
+        "Defines the argscope for your model."
+        "You can use a slim net to configure with fromSlim()",
+        typeConverter=TypeConverters.toString)
 
-    numGpusParam = Param(Params._dummy(),
-                    "gpus", "number of GPUs",
-                    typeConverter=TypeConverters.toInt)
+    netParam = Param(
+        Params._dummy(),
+        "net", "the net param."
+        "This is evaluated as a raw string with eval."
+        "You can use a slim net to configure with fromSlim()",
+        typeConverter=TypeConverters.toString)
 
-    layersParam = Param(Params._dummy(),
-                        "layers", "the layers"
-                        typeConverter=TypeConverters.toListString)
-
-    optimizerParam = Param(
-        Params._dummy()
-        'optimizer',
-        'The name of the optimizer, one of "adadelta", "adagrad", "adam",'
-        '"ftrl", "momentum", "sgd" or "rmsprop".')
-
-    loss
+    syncReplicasParam = Param(
+        Params._dummy(),
+        "syncReplicas", "if we should use the sync optimizer"
+        typeConverter=TypeConverters.toBoolean)
+    
     def __init__(self):
         self._cluster_args = None
         self._cluster = None
         self._setDefault(
-            stepsParam=1000, epochsParam=10, rdmaParam=True, numGpusParam=0,
-            optimizerParam="ftrl")
+            stepsParam=1000, epochsParam=10,
+            syncReplicasParam=False)
 
-    def _start_cluser(self):
+    def _start_training_cluser(self, num_classes):
         """
-        Start a TFCluster if one is not running.
+        Start a TFCluster if one is not running for a given number of classes.
         """
         if self._cluster:
             if self._cluster_params != self._generate_params():
@@ -69,7 +77,8 @@ class TensorFlowSlimEstimator(Model, HashInputCol, HasOutputCol):
             else:
                 return self._cluster
         self._cluster_params = self._generate_params()
-        layers_
+
+        sync_replicas_value = self.getSyncReplicas()
         def map_fun(args, ctx):
             from tensorflowonspark import TFNode
             import tensorflow as tf
@@ -88,7 +97,7 @@ class TensorFlowSlimEstimator(Model, HashInputCol, HasOutputCol):
                 time.sleep((worker_num + 1) * 5)
                 self._cluster = run(sc, map_fun, tf_args)
 
-            # Convert the batch
+            # Convert the batch, TODO use Arrow side here (Spark 2.3+)
             def feed_dict(batch):
                 featureVecs = []
                 labels = []
@@ -108,23 +117,36 @@ class TensorFlowSlimEstimator(Model, HashInputCol, HasOutputCol):
                 with tf.device(tf.train.replica_device_setter(
                         worker_device="/job:worker/task:%d" % task_index,
                         cluster=cluster)):
-                    eval(layers)
-                    global_step = slim.variables.global_step()
-                    num_batches_per_epoch = (
-                        dataset.num_examples_per_epoch() / batch_size)
-                    decay_steps = int(
-                        num_batches_per_epoch * num_epochs_per_decay /
-                        num_replicas_to_aggregate)
-                    
+                    with eval(arg_value):
+                        net = eval(net_value)
+                        total_loss = slim.losses.get_total_loss()
+                        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+                        train_op = slim.learning.create_train_op(total_loss, optimizer)
+                        logdir = os.mk
+                        slim.learning.train(
+                            train_op,
+                            logdir,
+                            master=server.target,
+                            is_chief=(FLAGS.task == 0),
+                            number_of_steps=steps,
+                            number_of_steps=num_steps,
+                            save_interval_secs=600,
+                            sync_optimizer=optimizer if sync_replicas_value else None)
+                        
                     
     def _generate_params(self):
         """
         Generate a lit of params.
         """
-        return [self.getSteps(), self.getEpochs()]
+        return [self.getSteps(), self.getEpochs(), self.getNetParam()]
 
-    def fit(self,):
-        self._start_cluster()
+    def fit(self, df):
+        selected_df = df.select(
+            df.col(self.getLabelCol()).cast(IntegerType()),
+            self.getFeaturesCol())
+        selected_df.cache()
+        num_classes = selected_df.agg(countDistinct(self.getLabelCol())).head()[0]
+        self._start_training_cluster(num_classes)
 
     def setSteps(self, value):
         """
@@ -150,26 +172,55 @@ class TensorFlowSlimEstimator(Model, HashInputCol, HasOutputCol):
         """
         return self.getOrDefault(self.epochParam)
 
-    def setRdma(self, value):
+    def setNet(self, value):
         """
-        Sets the value of :py:attr:`rdmaParam`.
+        Sets the value of :py:attr:`netParam`.
         """
-        return self._set(rdmaParam=value)
+        return self._set(netParam=value)
 
-    def getRdma(self, value):
+    def getNet(self, value):
         """
-        Get if rdma is enabled.
+        Get code for the net.
         """
-        return self.getOrDefault(self.rdmaParam)
+        return self.getOrDefault(self.netParam)
 
-    def setBaseNet(self, value):
+    def setArgScope(self, value):
         """
-        Sets the value of :py:attr:`baseNetParam`.
+        Sets the value of :py:attr:`argScopeParam`.
         """
-        return self._set(rdmaParam=value)
+        return self._set(argScopeParam=value)
 
-    def getBaseNet(self, value):
+    def getArgScope(self, value):
         """
-        Get name of the base network.
+        Get code for the arg scope.
         """
-        return self.getOrDefault(self.rdmaParam)
+        return self.getOrDefault(self.argScopeParam)
+
+    def fromSlim(self, package, net, scope):
+        """
+        Sets :py:attr:`netParam`. and :py:attr:`argScopeParam` for a given
+        slim net.
+        """
+        self.setNet("slim.nets.{0}.{1}(inputs, num_classes=num_classes)".format(package, net))
+        self.setArgScope("slim.nets.{0}.{1}()".format(package, scope))
+
+
+if __name__ == '__main__':
+    tfsle = TensorFlowSlimEstimator()
+    tfsle.setFeatureCol("features")
+    tfsle.setLabelCol("labels")
+    tfsle.fromSlim("vgg", "vgg_a", "vgg_arg_scope")
+    sc = SparkContext(conf=SparkConf().setAppName("tflowrun"))
+    spark = SparkSession.getOrCreate()
+    df = spark.createDataFrame(
+        [(1.0, 0.2, 1.0), (3.0, 0.2, 2.0), (4.0, 0.2, 2.0), (4.0, 0.5, 2.0)],
+        ["score", "junk", "values"])
+    from pypsark.ml.feature import VectorAssembler
+    assembler = VectorAssembler(inputCols=["score", "junk"], outputCol="feature")
+    assembled = assembler.transform(df)
+    tfse = TensorFlowSlimEstimator()
+    tfse.setFeatureCol("feature")
+    tfse.setLabelCol("values")
+    tfse.fit(assembled)
+    
+
